6b136e7b8f244cfccf12601ebf76567cf2fede00
Anthony Pang <anthonypang99@gmail.com>
2025-02-05 15:36:44 -0500

    render CreateAccountDrawer on dashboard

--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -1,7 +1,27 @@
+import CreateAccountDrawer from "@/components/createAccountDrawer";
+import { Card } from "@/components/ui/card";
+import { CardContent } from "@/components/ui/card";
+import { Plus } from "lucide-react";
 import React from "react";
 
 const page = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      {/* Budget Progress  */}
+      {/* Dashboard Overview */}
+      {/* Accounts Grid */}
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+        <CreateAccountDrawer>
+          <Card className="hover:shadow-md transition-shadow cursor-pointer duration-300 border-dashed">
+            <CardContent className="flex flex-col items-center justify-center text-muted-foreground h-full pt-5">
+              <Plus className="h-10 w-10 mb-2" />
+              <p className="text-sm font-medium">Add Account</p>
+            </CardContent>
+          </Card>
+        </CreateAccountDrawer>
+      </div>
+    </div>
+  );
 };
 
 export default page;
