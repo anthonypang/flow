bf6ba29fec68e48bdfd9701a5f671826184d5b35
Anthony Pang <anthonypang99@gmail.com>
2025-02-10 17:19:57 -0500

    comment out bot checking

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,4 @@
-import arcjet, { createMiddleware, detectBot, shield } from "@arcjet/next";
+// import arcjet, { createMiddleware, detectBot, shield } from "@arcjet/next";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
@@ -8,7 +8,7 @@ const isProtectedRoute = createRouteMatcher([
   "/transaction(.*)",
 ]);
 
-const clerk = clerkMiddleware(async (auth, req) => {
+export default clerkMiddleware(async (auth, req) => {
   const { userId } = await auth();
   if (isProtectedRoute(req)) {
     if (!userId) {
@@ -21,26 +21,26 @@ const clerk = clerkMiddleware(async (auth, req) => {
 });
 
 // Create Arcjet middleware
-const aj = arcjet({
-  key: process.env.ARCJET_KEY as string,
-  // characteristics: ["userId"], // Track based on Clerk userId
-  rules: [
-    // Shield protection for content and security
-    shield({
-      mode: "LIVE",
-    }),
-    detectBot({
-      mode: "LIVE", // will block requests. Use "DRY_RUN" to log only
-      allow: [
-        "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
-        "GO_HTTP", // For Inngest
-        // See the full list at https://arcjet.com/bot-list
-      ],
-    }),
-  ],
-});
+// const aj = arcjet({
+//   key: process.env.ARCJET_KEY as string,
+//   // characteristics: ["userId"], // Track based on Clerk userId
+//   rules: [
+//     // Shield protection for content and security
+//     shield({
+//       mode: "LIVE",
+//     }),
+//     detectBot({
+//       mode: "LIVE", // will block requests. Use "DRY_RUN" to log only
+//       allow: [
+//         "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
+//         "GO_HTTP", // For Inngest
+//         // See the full list at https://arcjet.com/bot-list
+//       ],
+//     }),
+//   ],
+// });
 
-export default createMiddleware(aj, clerk);
+// export default createMiddleware(aj, clerk);
 
 export const config = {
   matcher: [
